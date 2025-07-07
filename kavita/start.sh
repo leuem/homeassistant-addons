5f4ff764108a821e1253db396521b9486141d453
Yury Solgalov <yesolgalov@gmail.com>
2025-07-07 15:21:12 +0300

    Added kavita

--- kavita/start.sh
+++ kavita/start.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-mkdir -p /config/kavita
+UUID="8d94cfa4"
+SLUG="kavita"
+
+CFG_PATH="/config/addons_config/${UUID}_${SLUG}"
+
+mkdir -p "$CFG_PATH"
+rm -rf /kavita/config
+ln -s "$CFG_PATH" /kavita/config
 
 mkdir -p /share/kavita/books
 mkdir -p /share/kavita/manga
@@ -9,7 +16,5 @@ mkdir -p /share/kavita/comics
 [ -L /books ] || [ -d /books ] || ln -s /share/kavita/books /books
 [ -L /manga ] || [ -d /manga ] || ln -s /share/kavita/manga /manga
 [ -L /comics ] || [ -d /comics ] || ln -s /share/kavita/comics /comics
-
-rm -rf /kavita/config && ln -s /config/kavita /kavita/config
 
 exec /entrypoint.sh
